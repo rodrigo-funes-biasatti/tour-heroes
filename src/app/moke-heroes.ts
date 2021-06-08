91d924c70f487c3df0b19a4dea73812183151790
rodrigo-funes-biasatti <rodrigo11.funes@hotmail.com>
2021-06-07 21:47:01 -0300

    asd

---
+++ src/app/moke-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from '../hero';
+
+export const HEROES: Hero[] = 
+[
+    {id: 1, name: 'Juan Rizzo'},
+    {id: 2, name: 'Lucas Dinamarca'},
+    {id: 3, name: 'Nacho Chaparro'},
+    {id: 4, name: 'Geronimo Silva'},
+    {id: 5, name: 'Lautaro Neun'},
+    {id: 6, name: 'Maximo Funes'},
+    {id: 7, name: 'Valentino Del Pino'},
+]
